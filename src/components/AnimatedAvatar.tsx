d470ef2936dd9dcddaf4e9db3b43a358fd42a088
VARA4u-tech <pappuridurgavaraprasad4pl@gmail.com>
2026-06-04 09:04:50 +0530

    feat: add AnimatedAvatar component with brutalist styling and hover effects

--- src/components/AnimatedAvatar.tsx
+++ src/components/AnimatedAvatar.tsx
@@ -5,11 +5,11 @@ const AnimatedAvatar = () => {
 
   return (
     <div className="relative w-48 h-48 md:w-64 md:h-64 mx-auto md:mx-0 shrink-0 group">
-      {/* Brutalist offset shadow */}
-      <div className="absolute inset-0 border-2 border-black translate-x-2 translate-y-2 group-hover:translate-x-3 group-hover:translate-y-3 transition-transform duration-300" />
+      {/* Brutalist offset shadow (Solid Black) */}
+      <div className="absolute inset-0 bg-black translate-x-2 translate-y-2 group-hover:translate-x-4 group-hover:translate-y-4 transition-transform duration-300" />
 
       {/* Main frame */}
-      <div className="relative w-full h-full border-2 border-black bg-white overflow-hidden group-hover:-translate-x-0.5 group-hover:-translate-y-0.5 transition-transform duration-300">
+      <div className="relative w-full h-full border-2 border-black bg-white overflow-hidden group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300">
         <div className="w-full h-full animate-[float_6s_ease-in-out_infinite]">
           <img
             src="https://i.ibb.co/20jtF9Lm/Chat-GPT-Image-Feb-14-2026-10-13-36-PM.png"
@@ -18,8 +18,6 @@ const AnimatedAvatar = () => {
             loading="eager"
           />
         </div>
-
-        {/* Subtle scan line overlay */}
 
         {/* Subtle scan line overlay */}
         <div
@@ -32,8 +30,8 @@ const AnimatedAvatar = () => {
       </div>
 
       {/* Status badge */}
-      <div className="absolute -bottom-3 -right-3 bg-black text-white px-3 py-1 text-[10px] font-mono uppercase tracking-widest border border-black z-10">
-        <span className="inline-block w-1.5 h-1.5 bg-white rounded-none mr-1.5 animate-pulse" />
+      <div className="absolute -bottom-3 -right-3 bg-black text-white px-3 py-1 text-[10px] font-mono uppercase tracking-widest border border-black z-10 group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300">
+        <span className="inline-block w-1.5 h-1.5 bg-green-400 rounded-none mr-1.5 animate-pulse" />
         Available
       </div>
     </div>
